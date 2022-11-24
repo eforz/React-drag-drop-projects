c6bbd80f31473e9b2c51cd08ce9c55878511dfd2
Kirill <etxe.play@gmail.com>
2022-11-24 15:51:38 +0300

    add React-Router

---
+++ src/components/pages/PagesRouter/PagesRouter.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Route, Routes} from "react-router-dom";
+import ProjectPage from '../ProjectPage';
+import ProjectsPage from '../ProjectsPage';
+
+
+const PagesRouter = () => {
+  return (
+    <Routes>
+        <Route path='/' element={<ProjectsPage/>}> </Route>
+        <Route path='/project' element={<ProjectPage/>}> </Route>
+    </Routes>
+  )
+}
+
+export default PagesRouter
